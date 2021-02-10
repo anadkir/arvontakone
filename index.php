e5ee0e3860abc122ffc2911cd37e569bd324f53e
anadkir <64309347+anadkir@users.noreply.github.com>
2021-02-10 14:29:39 +0200

    Tehty logo ja taulukko

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fi">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <title>Arvontakone</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="content sign">
+            <h1 class="sign__word">Tervetuloa arvontaan</h1>
+            <?php include 'user-number.php' ?>
+            <div class="print-user-number">
+
+            </div>
+            <div class="comp-number">
+
+            </div>
+            <div class="result">
+
+            </div>
+        </div>
+    </body>
+</html>
---
+++ user-number.php
@@ -0,0 +1,97 @@
+<div class="user-number">
+    <div class="row-1">
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">1</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">2</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">3</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">4</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">5</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">6</label>
+    </div>
+    <div class="row-2">
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">7</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">8</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size1" for="btn-check-outlined">9</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">10</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">11</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">12</label>
+    </div>
+    <div class="row-3">
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary  btn-size2" for="btn-check-outlined">13</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary  btn-size2" for="btn-check-outlined">14</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary  btn-size2" for="btn-check-outlined">15</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary  btn-size2" for="btn-check-outlined">16</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary  btn-size2" for="btn-check-outlined">17</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary  btn-size2" for="btn-check-outlined">18</label>
+    </div>
+    <div class="row-4">
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">19</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">20</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">21</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">22</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">23</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">24</label>
+    </div>
+    <div class="row-5">
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">25</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">26</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">27</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">28</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">29</label>
+
+        <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+        <label class="btn btn-outline-primary btn-size2" for="btn-check-outlined">30</label>
+    </div>
+</div>
